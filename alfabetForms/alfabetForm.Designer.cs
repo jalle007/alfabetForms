edaebed670ff01dc157d3f80fbb40f0e8daff5ad
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-11-10 11:56:49 +0100

    first commit

---
+++ alfabetForms/alfabetForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace alfabetForms {
+    partial class alfabetForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.addNewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.textBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.buttonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.resetToolStripMenuItem,
+            this.addNewToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(640, 28);
+      this.menuStrip1.TabIndex = 3;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // editToolStripMenuItem
+      // 
+      this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+      this.editToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+      this.editToolStripMenuItem.Text = "Edit";
+      this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+      // 
+      // saveToolStripMenuItem
+      // 
+      this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+      this.saveToolStripMenuItem.Size = new System.Drawing.Size(52, 24);
+      this.saveToolStripMenuItem.Text = "Save";
+      this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+      // 
+      // resetToolStripMenuItem
+      // 
+      this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+      this.resetToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+      this.resetToolStripMenuItem.Text = "Reset";
+      this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+      // 
+      // addNewToolStripMenuItem
+      // 
+      this.addNewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.textBoxToolStripMenuItem,
+            this.buttonToolStripMenuItem});
+      this.addNewToolStripMenuItem.Enabled = false;
+      this.addNewToolStripMenuItem.Name = "addNewToolStripMenuItem";
+      this.addNewToolStripMenuItem.Size = new System.Drawing.Size(80, 24);
+      this.addNewToolStripMenuItem.Text = "Add new";
+      // 
+      // textBoxToolStripMenuItem
+      // 
+      this.textBoxToolStripMenuItem.Name = "textBoxToolStripMenuItem";
+      this.textBoxToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+      this.textBoxToolStripMenuItem.Text = "TextBox";
+      this.textBoxToolStripMenuItem.Click += new System.EventHandler(this.textBoxToolStripMenuItem_Click);
+      // 
+      // buttonToolStripMenuItem
+      // 
+      this.buttonToolStripMenuItem.Name = "buttonToolStripMenuItem";
+      this.buttonToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+      this.buttonToolStripMenuItem.Text = "Button";
+      this.buttonToolStripMenuItem.Click += new System.EventHandler(this.buttonToolStripMenuItem_Click);
+      // 
+      // timer1
+      // 
+      this.timer1.Interval = 2000;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // alfabetForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(640, 324);
+      this.Controls.Add(this.menuStrip1);
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "alfabetForm";
+      this.Tag = "36516451-11d4-4178-9202-624c8b845045";
+      this.Text = "alfabetForm";
+      this.Load += new System.EventHandler(this.alfabetForm_Load);
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+        #endregion
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem addNewToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem textBoxToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem buttonToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+    private System.Windows.Forms.Timer timer1;
+  }
+}
---
+++ alfabetForms/alfabetForm.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Threading;
+
+namespace alfabetForms {
+  public partial class alfabetForm : Form {
+    public alfabetForm() {
+      InitializeComponent();
+    }
+
+    private string controlsInfoStr;
+    private string formTitle;
+    public dbEntities db; 
+     Controls  ctrls;
+
+    private void alfabetForm_Load(object sender, EventArgs e)
+    {
+      //Guid g = Guid.NewGuid();
+      formTitle = Text;
+
+      checkForm( );
+    }
+
+    private void checkForm( )
+    {
+       db= new dbEntities();
+      ctrls = (from r in db.Controls
+        where r.FormID == (string) Tag
+        select r).SingleOrDefault();
+
+      bool BeingModified = (bool) ctrls.BeingModified;
+
+      if (BeingModified )
+      {
+        Text = formTitle+ " - ReadOnly";
+        loadControls();
+        menuStrip1.BackColor = Color.LightCoral;
+        menuStrip1.Enabled = false;
+        timer1.Enabled = true;
+      }
+      else if(!BeingModified) //enable form
+      {
+
+        resetControls();
+        formTitle =  Text;
+        menuStrip1.Enabled = true;
+        loadControls();
+        Invalidate();
+      //  timer1.Enabled = false;
+      }
+    }
+
+    private void loadControls()
+    {
+      foreach (Control ctrl in Controls) {
+        if (ctrl.GetType() == typeof(TextBox) || ctrl.GetType() == typeof(Button))
+          Controls.Remove(ctrl);
+      }
+      controlsInfoStr = ctrls.Controls1;
+      string[] controlsInfo = controlsInfoStr.Split(new[] {"*"}, StringSplitOptions.RemoveEmptyEntries);
+
+      foreach (string controlInfo in controlsInfo)
+      {
+        string[] info = controlInfo.Split(new[] {":"}, StringSplitOptions.RemoveEmptyEntries);
+        String ctrlName = info[0];
+        var val = info[1].Split(',').Select(Int32.Parse).ToList();
+        addCtrl(ctrlName, val[0], val[1], val[2], val[3]);
+      }
+    }
+
+    private void editToolStripMenuItem_Click(object sender, EventArgs e) {
+      menuStrip1.BackColor = Color.LightGreen;
+
+      //drawRect(Color.Green);
+      ctrls.BeingModified = true;
+      db.SaveChanges();
+
+      this.Text += " - Editing";
+      editToolStripMenuItem.Enabled = false;
+      addNewToolStripMenuItem.Enabled = true;
+
+      if (!string.IsNullOrWhiteSpace(controlsInfoStr)) {
+        ControlMoverOrResizer.SetSizeAndPositionOfControlsFromString(this, controlsInfoStr);
+      }
+
+      foreach (Control ctrl in Controls) {
+        if (ctrl.GetType() == typeof(TextBox) || ctrl.GetType() == typeof(Button))
+          ControlMoverOrResizer.Init(ctrl);
+      }
+      controlsInfoStr = ControlMoverOrResizer.GetSizeAndPositionOfControlsToString(this);
+    }
+
+    private void saveToolStripMenuItem_Click(object sender, EventArgs e) {
+      controlsInfoStr = ControlMoverOrResizer.GetSizeAndPositionOfControlsToString(this);
+      ctrls.Controls1 = controlsInfoStr;
+      ctrls.BeingModified = false;
+      db.SaveChanges();
+
+      resetControls();
+    }
+
+    private void resetControls() {
+      editToolStripMenuItem.Enabled = true;
+      Text = formTitle;
+      addNewToolStripMenuItem.Enabled = false;
+      Invalidate();
+      menuStrip1.BackColor = Color.Transparent;
+
+        ControlMoverOrResizer.Stop ( );
+    }
+
+  private void resetToolStripMenuItem_Click(object sender, EventArgs e) {
+      if (!string.IsNullOrWhiteSpace(controlsInfoStr)) {
+        ControlMoverOrResizer.SetSizeAndPositionOfControlsFromString(this, controlsInfoStr);
+        resetControls();
+      }
+    }
+
+    private void textBoxToolStripMenuItem_Click(object sender, EventArgs e) {
+      String newCtrl = getNextCtrlName("textbox");
+      addCtrl(newCtrl);
+      ControlMoverOrResizer.Init(Controls[newCtrl]);
+    }
+
+    private void buttonToolStripMenuItem_Click(object sender, EventArgs e) {
+      String newCtrl = getNextCtrlName("button");
+      addCtrl(newCtrl);
+      ControlMoverOrResizer.Init(Controls[newCtrl]);
+    }
+
+    private void addCtrl(String ctrlName, int posX= 100, int posY=100, int width=175,int height=30 ) {
+      dynamic ctrl = null;
+      if (ctrlName.ToLower().StartsWith("textbox"))
+      {
+      ctrl = new TextBox();
+      ctrl.Multiline = true;
+      }
+      
+      if (ctrlName.ToLower().StartsWith("button")) ctrl = new Button();
+     
+      ctrl.Name = ctrlName;
+      ctrl.Location = new Point(posX, posY);
+      ctrl.Size = new  Size(width, height);
+      ctrl.Text = ctrl.Name;
+      
+      Controls.Add(ctrl);
+      controlsInfoStr = ControlMoverOrResizer.GetSizeAndPositionOfControlsToString(this);
+    }
+
+    private string getNextCtrlName(String seed, int n=0)
+    {
+      String newName = seed + n;
+      foreach (Control ctrl in Controls) {
+        if (ctrl.Name.ToLower() == newName.ToLower())
+        {
+          return getNextCtrlName(seed, n + 1);
+          break;
+        }
+      }
+
+        return newName;
+    }
+
+    private void timer1_Tick(object sender, EventArgs e) {
+      checkForm( );
+    }
+  }
+}
